2ba0b1e8e5daa7fb6ace44e7fec5b7fca2ca5d59
yihong0618 <zouzou0208@gmail.com>
2020-03-06 09:38:44 +0800

    day1 for 7 days go

---
+++ learn_go/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"gee"
+)
+
+func main() {
+	r := gee.New()
+	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	})
+
+	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	})
+
+	r.Run(":9999")
+}
