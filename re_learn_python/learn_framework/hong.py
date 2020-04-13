230fc2ac821446d9ef000b8ff7622dac3d21f86d
yihong0618 <zouzou0208@gmail.com>
2020-04-13 11:34:10 +0800

    add: learn somthing

--- re_learn_python/learn_framework/hong.py
+++ re_learn_python/learn_framework/hong.py
@@ -1,14 +1,55 @@
 from wsgiref.simple_server import make_server
 
 
-def app(environ, start_response):
-    status = "200 OK"
-    headers = [("Content-type", "text/html"), ("Contetnt-length", "200")]
-    start_response(status, headers)
-    yield b"hello"
-    yield b"world"
+class Hong:
+
+    def __init__(self):
+        self.routes = {}
+
+    def __call__(self, environ, start_response):
+        status = "200 OK"
+        response_content = [("Content-type", "text/plain")]
+        path = environ["PATH_INFO"]
+        response_func = self.routes[path]
+        start_response(status, response_content)
+        return response_func(path)
+
+    def route(self, route):
+        def _(func):
+            self.routes[route] = func
+        return _
+
+
+class Request:
+    pass
+
+
+class Reponse:
+
+    def __init__(self, response):
+        self.response = response
+
+    def __iter__(self):
+        for val in self.response:
+            if isinstance(val, bytes):
+                yield val
+            else:
+                yield val.encode()
+
+h = Hong()
+
+
+@h.route("/hello")
+def hello(request):
+    name = request.args.get("name", "?")
+    return Reponse(f"Hello {name}")
+
+
+@h.route("/bye")
+def bye(request):
+    return Reponse("bye")
 
 
 if __name__ == "__main__":
-    s = make_server("127.0.0.1", 5433, app)
+    s = make_server("127.0.0.1", 5433, h)
     s.serve_forever()
