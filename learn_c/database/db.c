7d3141c5cc956f06643ef42eb4c2306fc7fa98d6
yihong0618 <zouzou0208@gmail.com>
2020-05-15 09:29:52 +0800

    learn c by db day2

--- learn_c/database/db.c
+++ learn_c/database/db.c
@@ -10,8 +10,62 @@ typedef struct {
     ssize_t input_length;
 } InputBuffer;
 
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECONIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+
+typedef struct {
+    StatementType type;
+} Statement;
+
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        close_input_buffer(input_buffer);
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_COMMAND_UNRECONIZED_COMMAND;
+    }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    } else {
+        return PREPARE_UNRECOGNIZED_STATEMENT;
+    }
+}
+
+void excute_statement(Statement* statement) {
+    switch(statement->type) {
+        case (STATEMENT_INSERT):
+            printf("--------insert----------\n");
+            break;
+        case (STATEMENT_SELECT):
+            printf("--------select----------\n");
+            break;
+    }
+}
+
+
 InputBuffer* new_input_buffer() {
-    InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+    InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
     input_buffer->buffer = NULL;
     input_buffer->buffer_length = 0;
     input_buffer->input_length = 0;
@@ -32,10 +86,6 @@ void get_input(InputBuffer* input_buffer) {
 }
 
 
-void close_input_buffer(InputBuffer* input_buffer) {
-    free(input_buffer->buffer);
-    free(input_buffer);
-}
 
 
 int main(int argc, char* argv[]) {
@@ -43,11 +93,30 @@ int main(int argc, char* argv[]) {
     while (1) {
         promt_print();
         get_input(input_buffer);
-        if (strcmp(input_buffer->buffer, ".exit") == 0) {
-            printf("haha");
-            exit(EXIT_SUCCESS);
-        } else {
-          printf("input is %s\n", input_buffer->buffer);
+        /* if (strcmp(input_buffer->buffer, ".exit") == 0) { */
+        /*     exit(EXIT_SUCCESS); */
+        /* } else { */
+        /*   printf("input is %s\n", input_buffer->buffer); */
+        /* } */
+        if (input_buffer->buffer[0] == 46) {
+            switch (do_meta_command(input_buffer)) {
+                case (META_COMMAND_SUCCESS):
+                    printf("--------test---------\n");
+                    continue;
+                case (META_COMMAND_UNRECONIZED_COMMAND):
+                    printf("--------unreconized--------\n");
+                    continue;
+            }
         }
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement)) {
+            case PREPARE_SUCCESS:
+                break;
+            case PREPARE_UNRECOGNIZED_STATEMENT:
+                printf("???????\n");
+                continue;
+        }
+        excute_statement(&statement);
     }
 }
+
---
+++ learn_c/enum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+typedef enum {
+    TEST1,
+    TEST2,
+    TEST3
+} TEST;
+
+int main() {
+    printf("%d\n", TEST3);
+    return 0;
+}
